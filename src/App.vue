58a7d3a6b5ca11e627e87d75aecda4f9d920b0a9
hyeon0714 <xodid6960@naver.com>
2024-04-11 17:38:29 +0900

    ...

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,17 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
-</template>
 
+  <router-view/>
+</template>
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import "@/assets/css/jblog.css"
 
 export default {
-  name: 'App',
-  components: {
-    HelloWorld
+  nmae: "App",
+  components: {},
+  data() {
+    return {
+
+    }
   }
 }
 </script>
-
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
---
+++ src/views/main/JblogMain.vue
@@ -0,0 +1,54 @@
+<template>
+	<div id="center-content">
+		
+		<!--메인 해더 자리 -->
+		<MainHeader/>
+
+		
+		<form id="search-form">
+			<fieldset>
+				<input type="text" name="keyword" >
+				<button id="btnSearch" type="submit" >검색</button>
+			</fieldset>
+			
+			<fieldset>
+				<label for="rdo-title">블로그 제목</label> 
+				<input id="rdo-title" type="radio" name="kwdOpt" value="optTitle" > 
+				
+				<label for="rdo-userName">블로거 이름</label> 
+				<input id="rdo-userName" type="radio" name="kwdOpt" value="optName" > 
+			</fieldset>
+		</form>
+		
+		<div id="resultList">
+			
+			
+		</div>
+		
+		<!-- 메인 푸터  자리-->
+		<MainFooter/>
+	
+	</div>
+	<!-- //center-content -->
+</template>
+
+<script>
+import MainHeader from '@/components/MainHeader.vue'
+import MainFooter from '@/components/MainFooter.vue'
+
+export default{
+        name: "JbolgMain",
+        components: {
+            MainHeader,
+			MainFooter
+        },
+        data(){
+            return{
+
+            }
+        },
+        methods: {
+            
+        }
+    }
+</script>
